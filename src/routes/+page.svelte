1a0d971aac7dd1433064897b474dfbb3d056a4e8
Carolina Roveda <rovecarolina@gmail.com>
2026-04-08 15:27:08 +0200

    Primo commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let count = $state(0)
+    let doubled = $derived(count * 2)
+
+    console.log("hello world")
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++}>
+    Clicks: {count}
+</button>
+
+<style> 
+    h1 {
+        color: deepskyblue;
+    }
+</style>
